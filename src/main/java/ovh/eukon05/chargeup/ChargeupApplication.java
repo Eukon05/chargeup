d8f955ee46dc3308458490181766e5ff3a041bba
Łukasz Konieczny <lukasz.konieczny.05@outlook.com>
2026-06-18 10:48:43 +0200

    Added Swagger and disabled CORS

--- src/main/java/ovh/eukon05/chargeup/ChargeupApplication.java
+++ src/main/java/ovh/eukon05/chargeup/ChargeupApplication.java
@@ -1,9 +1,18 @@
 package ovh.eukon05.chargeup;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+        info = @Info(
+                description = "ChargeUP is a service that provides information about the current energy mix in Great Britain and predicts optimal charging windows for EVs based on clean energy availability.",
+                title = "ChargeUP API",
+                version = "1.0"
+        )
+)
 public class ChargeupApplication {
 
     public static void main(String[] args) {
--- src/main/java/ovh/eukon05/chargeup/controller/EnergyController.java
+++ src/main/java/ovh/eukon05/chargeup/controller/EnergyController.java
@@ -1,17 +1,21 @@
 package ovh.eukon05.chargeup.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.hibernate.validator.constraints.Range;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ovh.eukon05.chargeup.dto.CurrentMixResponseDTO;
 import ovh.eukon05.chargeup.dto.OptimalChargeWindowResponseDTO;
 import ovh.eukon05.chargeup.service.EnergyService;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/api/v1/energy")
 @RequiredArgsConstructor
 @Validated
@@ -19,12 +23,43 @@ public class EnergyController {
     private final EnergyService energyService;
 
     @GetMapping("/current")
+    @Operation(summary = "Calculates the average daily energy sources share in the GB mix for three days, starting today")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully retrieved the current energy mix",
+                    content = {
+                            @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = CurrentMixResponseDTO.class))
+                    }
+            ),
+            @ApiResponse(responseCode = "503", description = "The external API was unavailable",
+                    content = {
+                            @Content(mediaType = "application/json", schema = @Schema(implementation = String.class))
+                    }
+            )
+    })
     public CurrentMixResponseDTO getCurrentMix() {
         return energyService.getCurrentMix();
     }
 
     @GetMapping("/window")
-    public OptimalChargeWindowResponseDTO getOptimalWindow(@RequestParam @Range(min = 1, max = 6) int windowLength) {
+    @Operation(summary = "Calculates a predicted window of time for two days after today, when the share of clean energy sources in the mix is the highest.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully calculated the window",
+                    content = {
+                            @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = OptimalChargeWindowResponseDTO.class))
+                    }),
+            @ApiResponse(responseCode = "503", description = "The external API was unavailable",
+                    content = {
+                            @Content(mediaType = "application/json", schema = @Schema(implementation = String.class))
+                    }
+            ),
+            @ApiResponse(responseCode = "400", description = "The provided windowLength parameter was out of the 1-6 range",
+                    content = {
+                            @Content(mediaType = "application/json", schema = @Schema(implementation = String.class))
+                    })
+    })
+    public OptimalChargeWindowResponseDTO getOptimalWindow(@Parameter(description = "How long the window should be, in hours") @RequestParam @Range(min = 1, max = 6) int windowLength) {
         return energyService.getOptimalChargeWindow(windowLength);
     }
 }
